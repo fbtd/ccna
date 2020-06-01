2de2ca6b5f8aa83b1a54734c045f04478f190cf0
fbtd <fabiano.taddei@gmail.com>
2020-06-01 20:41:05 +0200

    added load configuration option

--- cl/cl.py
+++ cl/cl.py
@@ -12,6 +12,8 @@ parser.add_argument("-d", "--debug", help="enable debugging output",
 parser.add_argument("-s", "--show-run", "--sh-run", dest="sh_run",
                                     help="show running configuration",
                                     action="store_true")
+parser.add_argument("-l", "--load", help="load configuration from stdin",
+                                     action="store_true")
 args = parser.parse_args()
 
 def set_nonblocking(fd):
@@ -28,15 +30,15 @@ set_nonblocking(p.stdout.fileno())
 state = "not connected"
 lines = list()
 
-def send(msg, check_echo=True, add_crnl=True):
-    print(f"sending: {msg}")
+def send(msg, sleep_time=0.5, check_echo=True, add_crnl=True):
+    print("sending: ", msg.rstrip("\r\n"))
     msg = msg.encode()
     if add_crnl: msg = msg + b"\r\n"
     p.stdin.write(msg)
     p.stdin.flush()
     # wait for echo
-    time.sleep(0.5)
     if len(msg) - 2 and check_echo:
+        time.sleep(sleep_time)
         echo = p.stdout.read(len(msg)-2)
         print(echo, msg[:-2])
         assert echo == msg[:-2], "wrong echo"
@@ -45,9 +47,9 @@ def get_line():
     if lines:
         p_out = lines.pop()
     else:
-        time.sleep(0.1)
+        time.sleep(0.05)
         p_out = p.stdout.readline().decode()
-    print(f"getting: {p_out}")
+    print("getting: ", p_out.rstrip("\r\n"))
     return p_out
 
 def put_line(msg):
@@ -73,7 +75,7 @@ def connect_priviledged():
     if l[-1] == '>':
         send("en")
         l = get_all_lines()[-1]
-    elif l[-1] != '#':
+    elif ")#" in l:
         send("end")
         l = get_all_lines()[-1]
 
@@ -84,9 +86,24 @@ def connect_priviledged():
 def print_running_config():
     send("sh run")
     time.sleep(5)
-    configuration = get_all_lines()[:-2]
+    configuration = get_all_lines()[4:-2]
     print(".........")
     op("".join(configuration))
 
+def load_config():
+    send("write erase")
+    get_all_lines()
+    send("conf t")
+    for line in sys.stdin:
+        line = line.rstrip("\r\n")
+        send(line, check_echo=False, sleep_time=0.1)
+        for ret_line in get_all_lines():
+            assert "Invalid input detected" not in ret_line
+            assert "Incomplete command" not in ret_line
+            assert "Ambiguous command" not in ret_line
+    time.sleep(1)
+    get_all_lines()
+
 connect_priviledged()
 if args.sh_run: print_running_config()
+if args.load: load_config()
